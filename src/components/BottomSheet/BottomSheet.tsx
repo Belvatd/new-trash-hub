ae48c5edab264bc387b6062d17c0d25c5a36614f
belvatalita <belvatalita28@gmail.com>
2026-06-30 09:11:25 +0700

    style: reduce max-width of BottomSheet backdrop and container to 360px and 340px respectively

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -33,7 +33,7 @@ export default function BottomSheet(props: TBottomSheet) {
           open ? { opacity: 0.3, zIndex: 9998 } : { opacity: 0, display: "none" }
         }
         initial={{ opacity: 0 }}
-        className="fixed bottom-0 top-0 left-1/2 -translate-x-1/2 h-full w-full max-w-[430px] bg-black"
+        className="fixed bottom-0 top-0 left-1/2 -translate-x-1/2 h-full w-full max-w-[360px] bg-black"
       />
       <AnimatePresence initial={false}>
         {open && (
@@ -47,7 +47,7 @@ export default function BottomSheet(props: TBottomSheet) {
               collapsed: { x: "-50%", y: "100%", height: 0 },
             }}
             transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
-            className="fixed bottom-3 left-1/2 z-[9999] w-[calc(100%-24px)] max-w-[406px] rounded-3xl bg-white"
+            className="fixed bottom-3 left-1/2 z-[9999] w-[calc(100%-24px)] max-w-[340px] rounded-3xl bg-white"
           >
             <div ref={bottomSheetRef} className={className}>
               {children}
